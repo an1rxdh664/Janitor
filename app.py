1ff14a8a797a6153616d63f3ba3600e8573c0ad7
an1rxdh664 <anirudhhh637@gmail.com>
2026-03-18 10:11:26 +0530

    Added : Diff text extraction from repositories

--- app.py
+++ app.py
@@ -8,10 +8,19 @@ from dotenv import load_dotenv
 import hmac, hashlib, base64
 from hmac import compare_digest
 
+import httpx
+import requests
+
 load_dotenv() # LOAD VARIABLES FROM .env
 WEBHOOK_SECRET = os.getenv("WEBHOOK_SECRET")
+GITHUB_AUTH_TOKEN = os.getenv("GITHUB_AUTH_TOKEN")
 
-description = "This API endpoint is currently made for testing purposes of my janitor model."
+headers = {
+    "Accept: application/vnd.github+json",
+    f"Authorization: Bearer {GITHUB_AUTH_TOKEN}"
+}
+
+description = "This API endpoint is currently made only for testing purposes of my janitor model."
 
 app = FastAPI(
     title="Webhook Endpoint tester",
@@ -38,26 +47,37 @@ def verify_request(headers, body, secret: str):
 
     return compare_digest(computated_signature, signature_header)
 
-def event_logger(body_json):
+def event_logger(body_json, diff_data):
     print("Inside event logger\n")
-    try:            
+    try:
             pr_number = body_json.get("number")
             repository_full_name = body_json.get("repository").get("full_name")
             action_type = body_json.get("action")
             diff_url = body_json.get("pull_request").get("diff_url")
             head_sha = body_json.get("pull_request").get("head").get("sha")
             
+            print(f"Repository : {body_json.get("repository")}\n")
+
             print("Details of the PR : \n")
             print(f"Pull request number : {pr_number}")
         
             print(f"Full Repository Name : {repository_full_name} | Action : {action_type}\n")
-            print(f"Diff URL : {diff_url}\nHead SHA : {head_sha}\n")
+            print(f"Diff URL : {diff_url}\nHead SHA : {head_sha}\nDiff Data : {diff_data}")
+            # CHECKING IF THE DIFF DATA IS GETTING RECIEVED OR NOT NOT COMPLETED YET
 
     except Exception as e:
         return {"Error": "Some error occured while fetching data"}
     print("\nLogging done\n")
-    
+
     return
+
+async def fetch_diff_data(diff_url):
+    response = requests.get(diff_url)
+
+    if response.status_code == 200:
+        return response.text
+    else:
+        return {"error": "could not fetch diff data"}
 
 @app.get("/")
 async def server_info():
@@ -94,10 +114,13 @@ async def get_webhook(request: Request):
             # EVENT GATING
             elif(event == "pull_request"):
                 # But first the body needs to be decoded into the utf-8 standard encoding
-                body_bytes_decoded = req_body_bytes.decode("utf-8")
-                body_json = json.loads(body_bytes_decoded)
+                decodedBodyBytes = req_body_bytes.decode("utf-8")
+                jsonBody = json.loads(decodedBodyBytes)
 
-                event_logger(body_json)
+                diff_data = await fetch_diff_data(jsonBody.get("pull_request").get("diff_url"))
+                
+                # OPTIONAL FUNCTION TO LOG EVENTS
+                event_logger(jsonBody, diff_data)
 
                 return {"message": "Event was a PR"}
             else:
