07984870e0b08a0c5d21d3e8eca0993520e33422
an1rxdh664 <anirudhhh637@gmail.com>
2026-04-02 20:46:08 +0530

    Updated: Processing Logic, Added: BackgroundTasks Functionality

--- app.py
+++ app.py
@@ -1,10 +1,6 @@
-import json
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, BackgroundTasks
 
-from core.security import verify_request
-from core.github_client import fetch_diff_data, createURL, regenerate_description, send_patch
-from utils.logger import event_logger
-from ai.engine import generate_summary
+from core.processor import receive_data, construct_data
 
 description = "This API endpoint is currently made only for testing purposes of my janitor model."
 
@@ -31,52 +27,18 @@ async def server_info():
     }
 
 @app.post("/webhook")
-async def get_webhook(request: Request):  
+async def get_webhook(request: Request, background: BackgroundTasks):  
     try:
-        req_headers = request.headers        
-        req_body_bytes = await request.body()
-
-        request_status = verify_request(req_headers, req_body_bytes)
+        received_data = await receive_data(request)
         
-        decoded_request_body = req_body_bytes.decode("utf-8") 
-        request_body = json.loads(decoded_request_body)
+        background.add_task(construct_data, {
+            "body": received_data["body"],
+            "event": received_data["event"],
+            "action": received_data["action"],
+            "status": received_data["status"]
+        })
         
-        if(request_status):
-            event = req_headers.get("x-github-event")
-            action = request_body.get("action")
-
-            if(event == "ping"):
-                return {"message": "Event was a ping event"}
-            
-            if(action == "closed"):
-                return {"message": "The PR was closed"}
-            
-            elif(action == "edited"):
-                return {"message": "The PR information was edited"}
-            
-            elif(event == "pull_request" and (action == "opened" or action == "synchronize")):
-                url = createURL(request_body)                
-                data = fetch_diff_data(url)
-                
-                generated_summary = generate_summary(data)
-                pr_description_body = request_body.get("pull_request").get("body")
-
-                regenerated_pr_body = regenerate_description(pr_description_body, generated_summary)
-
-                patch_response = send_patch(url, regenerated_pr_body)
-
-                if patch_response == 401:
-                    return {"message": f"{patch_response}: Bad Token"}
-                elif patch_response == 403:
-                    return {"message": f"{patch_response}: Permission Issue"}
-                elif patch_response == 422:
-                    return {"message": f"{patch_response}: Invalid Payload"}
-
-                return {"message": f"{patch_response} : Successfull"}
-            else:
-                return {"message": "Event was neither a pr nor a ping event"}    
-        else:
-            return {"Validation Error": "The secret key could not be validated"}
-
+        return {"message": "Request Recieved, Background processing started"}
+    
     except Exception as e:
         return {"Error": "Invalid Data", "details": str(e)}
---
+++ core/processor.py
@@ -0,0 +1,105 @@
+import json, requests
+
+from core.security import verify_request
+from core.github_client import createURL, fetch_diff_data, regenerate_description
+from ai.engine import generate_summary
+
+from config import GITHUB_FINE_GRAINED_TOKEN
+
+# FUNCTION TO RECIEVE DATA FROM GITHUB
+async def receive_data(request):
+    req_headers = dict(request.headers)        
+    req_body_bytes = await request.body()
+
+    request_status = verify_request(req_headers, req_body_bytes)
+
+    if not request_status:
+        return {
+            "status": False,
+            "error": "Invalid Signature"
+        }
+    
+    decoded_request_body = req_body_bytes.decode("utf-8") 
+    request_body = json.loads(decoded_request_body)
+
+    event = req_headers.get("x-github-event")
+    action = request_body.get("action")
+
+    return {
+        "headers": req_headers,
+        "status": request_status,
+        "body": request_body,
+        "event": event,
+        "action": action
+    }
+
+
+# FUNCTION TO SEND IT TO GITHUB
+def send_data(url, body):
+    response = requests.patch(
+            url = url,
+            headers = {
+                "Authorization": f"Bearer {GITHUB_FINE_GRAINED_TOKEN}",
+                "Accept": "application/vnd.github+json",
+                "Content-Type": "application/json"
+            },
+            json = {
+                "body": body
+            },
+            timeout=15
+        )
+    
+    return response.status_code
+
+# FUNCTION TO GENERATE SUMMARY AND DESCRIPTION
+def construct_data(data):
+
+    status = data.get('status')
+    body = data.get('body')
+    event = data.get('event')
+    action = data.get('action')
+    
+    if(status):
+        if(event == "ping"):
+            print("Event was a ping event")
+            return
+        
+        if(action == "closed"):
+            print("The PR was closed")
+            return
+        
+        elif(action == "edited"):
+            print("The PR information was edited")
+            return
+        
+        elif event == "pull_request" and action in ["opened", "synchronize"]:
+            url = createURL(body)                
+            data = fetch_diff_data(url)
+            
+            generated_summary = generate_summary(data)
+            pr_description_body = body.get("pull_request").get("body")
+
+            regenerated_pr_body = regenerate_description(pr_description_body, generated_summary)
+
+            patch_response = send_data(url, regenerated_pr_body)
+            
+            if patch_response == 401:
+                print(f"{patch_response}: Bad Token")
+                return
+            elif patch_response == 403:
+                print(f"{patch_response}: Permission Issue")
+                return
+            elif patch_response == 422:
+                print(f"{patch_response}: Invalid Payload")
+                return
+            
+            print(f"{patch_response} : Successfull")
+            return
+        
+        else:
+            print("Event was neither a pr nor a ping event")
+            return    
+    
+    else:
+        print("The secret key could not be validated")
+        return
